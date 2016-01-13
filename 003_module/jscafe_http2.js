6fff871e603356e9629de71d789209f92a0956e5
fnwiya <fnwiya@gmail.com>
2016-01-14 07:10:36 +0900

    complate 003

---
+++ 003_module/jscafe_http2.js
@@ -0,0 +1,29 @@
+var http = require('http');
+var emitter = require('events').EventEmitter;
+var util = require('util');
+
+function JSCafe() {
+    emitter.call(this);
+}
+
+util.inherits(JSCafe, emitter);
+
+JSCafe.prototype.request = function (url) {
+    var self = this;
+    http.get(url, function (res) {
+        res.setEncoding('utf-8');
+        var buffer = '';
+        res.on('readable', function () {
+            buffer += res.read();
+        });
+        res.on('end', function () {
+            self.emit('end', buffer);
+        });
+        res.on('error', function (e) {
+            self.emit('error', e);
+        });
+    });
+    return self;
+};
+
+module.exports = JSCafe;
